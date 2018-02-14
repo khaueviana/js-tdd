ae7a29c6640aa326da5e34d2745844d180c55167
khaueviana <khaue.viana@concrete.com.br>
2018-02-14 15:11:58 -0200

    Add test sample

---
+++ tests/main.spec.js
@@ -0,0 +1,36 @@
+describe('Main', () => {
+  describe('Method A', () => {
+    context('Case 1', () => {
+      // it.skip('should happen blabla', () => {
+      it('should happen blabla', () => {
+        // espera que aconteça
+        // Entrada de dados / método sum(2,2)
+        // Espera retornar true ou falso
+        throw new Error('just an error');
+      });
+    });
+
+    // context.only('Case 2', () => {
+    context('Case 2', () => {
+      it('should happen blabla', () => {
+        // espera que aconteça
+        // Entrada de dados / método sum(2,2)
+        // Espera retornar true ou falso
+      });
+
+      it('should happen mimi', () => {
+        // espera que aconteça
+        // Entrada de dados / método sum(2,2)
+        // Espera retornar true ou falso
+      });
+    });
+  });
+
+  describe('Method B', () => {
+    it('should happen blabla', () => {
+      // espera que aconteça
+      // Entrada de dados / método sum(2,2)
+      // Espera retornar true ou falso
+    });
+  });
+});
