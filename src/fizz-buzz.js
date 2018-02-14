eb7957018b1f5be87ea6d8a5c0f4c6bc20bc0032
khaueviana <khaue.viana@concrete.com.br>
2018-02-14 17:10:54 -0200

    Add test coverage

--- src/fizz-buzz.js
+++ src/fizz-buzz.js
@@ -1,4 +1,5 @@
 const FizzBuzz = (num) => {
+  if (num === 0) return 0;
   if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
   if (num % 3 === 0) return 'Fizz';
   if (num % 5 === 0) return 'Buzz';
--- tests/fizz-buzz.spec.js
+++ tests/fizz-buzz.spec.js
@@ -19,4 +19,8 @@ describe('FizzBuzz', () => {
   it('should return the number when non-multiple', () => {
     expect(FizzBuzz(7)).to.be.equal(7);
   });
+
+  it('should return 0 when 0', () => {
+    expect(FizzBuzz(0)).to.be.equal(0);
+  });
 });
